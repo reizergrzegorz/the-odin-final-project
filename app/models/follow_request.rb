3babcd1409e2f9f84c1e233c9b0fe40d56b4acea
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-17 14:47:34 +0200

    add statuses and validation to FollowRequest model

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,4 +1,10 @@
 class FollowRequest < ApplicationRecord
-  belongs_to :requester
-  belongs_to :receiver
+  belongs_to :requester, class_name: "User"
+  belongs_to :receiver, class_name: "User"
+
+  STATUSES = %w[pending accepted rejected]
+
+  validates :status, inclusion: { in: STATUSES }
+  validates :requester_id, uniqueness: { scope: :receiver_id, message: "Already sent" }
 end
+
