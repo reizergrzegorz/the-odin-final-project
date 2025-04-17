997a469e20cefbbd13269e87c5c381b77af2e905
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-17 10:28:30 +0200

    add create method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
-end
+    
+  
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.build(comment_params)
+      @comment.user = current_user
+  
+      if @comment.save
+        redirect_to posts_path, notice: "Comment added"
+      else
+        redirect_to posts_path, alert: "Could not add comment"
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+  end
+  
