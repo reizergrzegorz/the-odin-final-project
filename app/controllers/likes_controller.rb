7a0f42b4c97aa705805a68eec3348c2b8bc2145a
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-17 11:51:34 +0200

    add create and destroy method in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
-end
+    before_action :set_post
+  
+    def create
+      @post.likes.create(user: current_user)
+      redirect_to post_path(@post)
+    end
+  
+    def destroy
+      @post.likes.where(user: current_user).destroy_all
+      redirect_to post_path(@post)
+    end
+  
+    private
+  
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+  end
+  
