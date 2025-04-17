ce65471a3fed0589dfc5104526e1b9fdf1ee17f9
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-17 12:59:13 +0200

    add methods to profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,31 @@
 class ProfilesController < ApplicationController
+
+  before_action :set_profile, only: [:show, :edit, :update]
+
   def show
   end
 
   def edit
+    redirect_to root_path, alert: "Could not edit someone profile" unless @profile.user == current_user
   end
 
   def update
+    if @profile.user == current_user && @profile.update(profile_params)
+      redirect_to profile_path(@profile), notice: "Profile updated"
+    else
+      flash.now[:alert] = "Could not save changes"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :bio, :avatar)
   end
 end
+
