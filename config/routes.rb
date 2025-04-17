0183e1e049ac7803710fd01315726c921e003bda
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-17 11:16:50 +0200

    add like resources

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   
   resources :posts, only: [:index, :new, :create, :show] do
     resources :comments, only: [:create]
-  end  
+  end 
+  resources :posts do
+    resource :like, only: [:create, :destroy]
+  end 
 end
